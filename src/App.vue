81b2e20c5762575817856ae9fc501b00a22a1a5b
Tomoki Horota <thorota@bltinc.co.jp>
2021-12-01 17:36:58 +0900

    reservの遷移削除

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">reserv</router-link>
+    <!-- <router-link to="/about">reserv</router-link> -->
   </div>
   <router-view/>
 </template>
